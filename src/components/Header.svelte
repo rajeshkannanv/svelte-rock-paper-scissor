ac4ba514b18dc8c105d88eccf76db799c276f89a
Rajesh Kannan <5030459+rajeshkannanv@users.noreply.github.com>
2020-07-20 14:48:30 +0530

    Add files via upload
    Initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,88 @@
+<script>
+  export let score;
+</script>
+
+<style>
+  .header-game {
+    border: 2px solid hsl(217, 16%, 45%);
+    border-radius: 20px;
+    width: 700px;
+    display: flex;
+    justify-content: space-between;
+    margin: 50px 0;
+  }
+
+  .header-game-name {
+    margin: 20px 30px;
+    color: #fff;
+    font-family: "Barlow Semi Condensed", sans-serif;
+    width: 160px;
+    text-transform: uppercase;
+    line-height: 0.8;
+    font-size: 1.5rem;
+  }
+
+  .header-game-score {
+    padding: 10px 30px;
+    text-align: center;
+    margin: 16px 20px;
+    background: #fff;
+    width: 150px;
+    border-radius: 10px;
+  }
+
+  .header-game-score span {
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    color: hsl(229, 64%, 46%);
+    letter-spacing: 2px;
+  }
+
+  .header-game-score p {
+    font-size: 4rem;
+    font-weight: 700;
+    color: hsl(229, 25%, 31%);
+  }
+
+  @media screen and (max-width: 575px) {
+    .header-game {
+      border: 1px solid hsl(217, 16%, 45%);
+      width: 315px;
+      margin: 26px;
+    }
+
+    .header-game-name {
+      line-height: 0.8;
+      font-size: 12px;
+      font-weight: 500;
+      width: 20px;
+    }
+
+    .header-game-score {
+      background: #fff;
+      width: 78px;
+    }
+
+    .header-game-score span {
+      display: flex;
+      justify-content: center;
+      text-align: center;
+      font-size: 0.5rem;
+    }
+
+    .header-game-score p {
+      font-size: 2rem;
+      font-weight: 700;
+    }
+  }
+</style>
+
+<div class="header-game">
+  <div class="header-game-name">
+    <h1>Rock Paper Scissors</h1>
+  </div>
+  <div class="header-game-score">
+    <span>Score</span>
+    <p>{score || '-'}</p>
+  </div>
+</div>
---
+++ src/components/Result.svelte
@@ -0,0 +1,136 @@
+<script>
+  export let userPicked = "";
+  export let machinePicked = "";
+  export let resultMessage;
+  export let onPlayagain;
+  export let visible;
+</script>
+
+<style>
+  .game {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .game div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .game p {
+    font-size: 1.5rem;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .picked {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    border-radius: 50%;
+    height: 300px;
+    width: 300px;
+    box-shadow: inset 0 -10px rgba(0, 0, 0, 0.15);
+    margin: 50px;
+  }
+
+  .picked > div {
+    height: 220px;
+    width: 220px;
+    background: #fff;
+    border-radius: 50%;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: inset 0 10px rgba(0, 0, 0, 0.15);
+  }
+
+  .game img {
+    height: 120px;
+    width: 120px;
+  }
+
+  .result > button {
+    width: 220px;
+    height: 56px;
+    color: hsl(349, 71%, 52%);
+    text-transform: uppercase;
+    font-family: "Barlow Semi Condensed", sans-serif;
+    font-size: 1rem;
+    letter-spacing: 2px;
+    border-radius: 10px;
+  }
+
+  .result > span {
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 3rem;
+    letter-spacing: 2px;
+    margin-bottom: 15px;
+  }
+
+  @media screen and (max-width: 575px) {
+    .game > div {
+      display: flex;
+      flex-direction: column-reverse;
+    }
+
+    .game p {
+      font-size: 1rem;
+      color: #fff;
+      text-transform: uppercase;
+    }
+
+    .picked {
+      height: 130px;
+      width: 130px;
+      margin: 20px;
+    }
+
+    .picked > div {
+      height: 100px;
+      width: 100px;
+    }
+
+    .game img {
+      height: 50px;
+      width: 50px;
+    }
+
+    .result {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 380px;
+    }
+  }
+</style>
+
+<div class="game">
+  <div>
+    <p>You picked</p>
+    <div class="picked button-{userPicked}">
+      <div class="circle">
+        <img src="./img/icon-{userPicked}.svg" alt={userPicked} />
+      </div>
+    </div>
+  </div>
+  {#if resultMessage}
+    <div class="result">
+      <span id="resultText">{resultMessage}</span>
+      <button id="playagain" on:click={onPlayagain}>Play again</button>
+    </div>
+  {/if}
+  <div>
+    <p>The Machine picked</p>
+    <div class="picked button-{machinePicked}">
+      <div class="circle">
+        <img src="./img/icon-{machinePicked}.svg" alt={machinePicked} />
+      </div>
+    </div>
+  </div>
+</div>
